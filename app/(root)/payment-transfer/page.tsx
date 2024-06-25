d2b3839d5f9ea174ad6b3baf5c95501b62f4a6fc
ujjwalshindee <ujjwalshindee@gmail.com>
2024-06-26 01:39:20 +0530

    Implement the complete layout

---
+++ app/(root)/payment-transfer/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Transfer = () => {
+  return (
+    <div>
+      transfer
+    </div>
+  )
+}
+
+export default Transfer
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransactionHistory = () => {
+  return (
+    <div>
+      transaction history
+    </div>
+  )
+}
+
+export default TransactionHistory
